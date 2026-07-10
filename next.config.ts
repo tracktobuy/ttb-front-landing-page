dcef77275502f05da3dc8329828b1c47c351c4fe
George Nunes <georgebnunes@gmail.com>
2026-07-10 10:16:08 -0300

    feat: deploy to develop pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
